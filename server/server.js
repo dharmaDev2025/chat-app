b9a89e789e49707a80dbb019b25e73715eb6dfdb
Dharmendra Behera <dharmendrabehera680@gmail.com>
2026-03-21 08:57:46 +0530

    done

--- server/server.js
+++ server/server.js
@@ -46,8 +46,15 @@ app.use("/api/messages",messageRouter)
 
 //connect to mongodb
 await connectDB();
-const PORT=process.env.PORT||1000;
+
+if(process.env.NODE_ENV!=="production){
+   const PORT=process.env.PORT||5000;
 server.listen(PORT,()=>{
     console.log("Server is running on the port :"+PORT);
 })
+   
+}
+//Export server for vercel
+export default server
+
 
